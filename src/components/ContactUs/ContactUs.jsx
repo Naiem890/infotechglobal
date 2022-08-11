47a4d008a7ae38dd486f3a9fcf50bc27f3c729ef
Naiem890 <solaiman.naiem890@gmail.com>
2022-08-11 18:16:23 +0600

    map integration done

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Banner from "../Shared/Banner";
 import ContactBannerImg from "./../../assets/contact-banner.jpg";
 import ContactBox from "./ContactBox";
+import MapLocation from "./MapLocation";
 
 const ContactUs = () => {
   return (
     <div>
       <Banner BannerImg={ContactBannerImg} BannerText={"Contact Us"} />
       <ContactBox />
+      <MapLocation />
     </div>
   );
 };
---
+++ src/components/ContactUs/MapLocation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MapLocation = () => {
+  return (
+    <div className="m-3 mt-0">
+      <iframe
+        title="Info-tech Location"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d21531.528224960395!2d78.35507343682116!3d17.446671373143044!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb934f05e38dcb%3A0xb0f9223212a6d327!2sINFOTECH%20GLOBAL%20PVT%20LTD!5e0!3m2!1sen!2sbd!4v1660219785485!5m2!1sen!2sbd"
+        width="100%"
+        height="520"
+        loading="lazy"
+      ></iframe>
+    </div>
+  );
+};
+
+export default MapLocation;
