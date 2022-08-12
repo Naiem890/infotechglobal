b269c57cf785a51fdbabb97a01fe223fdc9c84bd
Naiem890 <solaiman.naiem890@gmail.com>
2022-08-13 01:33:02 +0600

    mobile solution details page done

--- src/components/Services/MSService.jsx
+++ src/components/Services/MSService.jsx
@@ -1,7 +1,229 @@
 import React from "react";
+import TLi from "../Shared/TLi";
 
 const MSService = () => {
-  return <div>mobile solution</div>;
+  return (
+    <div>
+      <h2 className="text-4xl font-poppins font-bold mb-7">
+        Mobility Solutions & Services
+      </h2>
+      <h5 className="text-lg font-poppins font-bold mb-6">
+        Mobile Apps, the Internet of Things (IoT) and "comfortable computing"
+        are primed to dominate the industry year on year.
+      </h5>
+      <div className="mb-8 leading-7">
+        In today’s sophisticated world, there is a great demand for secure and
+        user centric mobile applications. IGI serves numerous mobile solutions
+        to its customers on a variety of platforms and devices ranging from iOS,
+        Windows, Android, HTML5, and many more. Our latest, cost-effective, good
+        design and high quality mobility solutions are highly researched, worked
+        upon, created and represented to the customers.
+      </div>
+      <h3 className="text-[28px] font-poppins font-bold mb-6">Challenges</h3>
+      <ul className="list-disc list-inside leading-7 mb-8">
+        <li>
+          This is generally the biggest concern for businesses when it comes to
+          deploying an enterprise mobility solution. While Bring Your Own Device
+          (BYOD) has brought so many benefits for businesses, including the
+          likes of increased productivity and cost saving, it has also brought
+          with it many security challenges.
+        </li>
+        <li>
+          Adopting a BYOD strategy can lead to security risks, especially among
+          enterprise CIO’s. Keeping sensitive corporate information secure is
+          critical and therefore you can’t be too careful when it comes to BYOD.
+        </li>
+        <li>
+          This is where enterprise mobility management solutions come in -
+          harnessing desktop and app virtualization can ensure all business
+          information is where it should be - in the data centre - accessed by
+          the people that need to access it, and not stored on an endpoint
+          device that could get misplaced on the tube or stolen from a handbag.
+        </li>
+        <li>
+          A good MAM (Mobile Application Management) solution will offer your
+          enterprise flexibility and your IT department to apply different
+          policies for different applications, user types and mobility
+          scenarios.
+        </li>
+        <li>Providing Mobile Data Access</li>
+        <TLi>
+          It’s one thing mobilizing your workforce, however, employees need to
+          be able to access company data on the go in order to make quick and
+          decisive decisions.
+        </TLi>
+        <TLi>
+          Employees need access to the data that’s available in the office. Live
+          data access from mobile devices will ensure your business and
+          employees remain effective and efficient.
+        </TLi>
+        <TLi>
+          But once again, this does present some more security concerns.
+          Enterprises must enable users to easily share data inside and outside
+          the business with secure file sharing that’s integrated with backup.
+        </TLi>
+      </ul>
+      <h3 className="text-[28px] font-poppins font-bold mb-6">
+        App Distribution
+      </h3>
+      <ul className="mb-8">
+        <TLi>
+          Making enterprise apps easily accessible can be quite complicated,
+          from installing directly to each device to providing employees and
+          users with tools to install themselves. This has created issues with
+          managing UDID (Unique Device Identification) numbers to changing
+          devices and more common BYOD policies.
+        </TLi>
+        <TLi>
+          More and more often, businesses are employing MAM services to manage
+          app accessibility for users and the security of the content within the
+          apps. Distribution needs to be thought through thoroughly to ensure
+          all users have access to the app when required. Some enterprise
+          applications can even be deployed on the consumer store, but be aware
+          as anybody can then utilize the app. Therefore a suitable login
+          process needs to be implemented.
+        </TLi>
+      </ul>
+      <h3 className="text-[28px] font-poppins font-bold mb-6">
+        Multi-Platform Mobile App Development & Testing
+      </h3>
+      <ul className="mb-8">
+        <TLi>
+          In today's BYOD world, one of the major hurdles to mobility cited by
+          IT professionals is around multi-platform mobile app development and
+          testing. Hence mobile application development backlogs continue to
+          grow, impacting employee productivity, customer service and
+          competiveness while key business initiatives continue to be delayed or
+          abandoned altogether.
+        </TLi>
+        <TLi>
+          The reason why there is such a chasm between demand for mobile apps
+          and ability to deliver is the shortage of skilled resources that
+          code-based development demands, particularly when it comes to
+          supporting multiple mobile platforms and devices.
+        </TLi>
+      </ul>
+      <h3 className="text-[28px] font-poppins font-bold mb-6">
+        Keeping Up with Technology
+      </h3>
+      <ul className="mb-8">
+        <TLi>
+          Once you've got your application up and running, the ongoing process
+          of running it will throw up additional problems.
+        </TLi>
+        <TLi>
+          Poor performance and speed are deal-breakers for a company when it
+          comes to their enterprise apps. While some businesses would argue that
+          part of this lies out of their hands - as the strength of data
+          networks is of course not their responsibility - it will fall to them
+          to develop apps that fulfil employees' requirements for functionality
+          without being so clunky as to always require above average network
+          connectivity to work.
+        </TLi>
+        <TLi>
+          This may extend to providing users with some offline functionality or
+          background synchronization options in instances when network
+          performance is lacking.
+        </TLi>
+        <TLi>
+          By abstracting back-end functions in the cloud, businesses can free up
+          their resources to simultaneously focus on front-end development and
+          develop strong mobile business models.
+        </TLi>
+      </ul>
+      <h3 className="text-[28px] font-poppins font-bold mb-6">
+        IGLIND Advantage
+      </h3>
+      <ul className="mb-8">
+        <TLi>Differentiated Mobility Architectures</TLi>
+        <TLi>Strategic mobility consulting capability</TLi>
+        <TLi>User interface design capability</TLi>
+        <TLi>
+          Dedicated Collaboration Center and a state-of-the-art mobile usability
+          lab
+        </TLi>
+        <TLi>
+          Ability to integrate across standard and proprietary back-end systems
+        </TLi>
+      </ul>
+      <h5 className="text-lg font-poppins font-bold mb-6">
+        Our Mobility solutions help you make informed and intelligent decisions
+        to achieve better business results. We enable you to experience a
+        connected ecosystem centred on:
+      </h5>
+      <div className="mb-8 leading-7">
+        <span className="font-bold">Employee Services -</span> for productivity
+        improvement (up to 30%) through simple workflows and optimized processes
+      </div>
+      <div className="mb-8 leading-7">
+        <span className="font-bold">Field Services -</span> for seamless and
+        affordable access to business critical information anywhere, anytime
+      </div>
+      <div className="mb-8 leading-7">
+        <span className="font-bold">Consumer Services -</span> for a unique
+        customer experience through intuitive and personal interactions across
+        touch points
+      </div>
+      <h3 className="text-[28px] font-poppins font-bold mb-6">Offerings</h3>
+      <div className="mb-8 leading-7">
+        The services offered cover all major mobile platforms including iOS,
+        Android, Windows Phone, Windows 8, Blackberry, HTML5, SAP-SUP and
+        associated server technologies, with scale and scope. We have repeatedly
+        demonstrated the ability to deliver operational efficiency and use
+        mobility to help you develop a competitive advantage.
+      </div>
+      <h5 className="text-lg font-poppins font-bold mb-6">Strategy Services</h5>
+      <ul className="mb-8">
+        <TLi>
+          Define mobility roadmap and identify key success factors for higher
+          user adoption
+        </TLi>
+        <TLi>
+          Determine the best technology platforms, architectures and components
+          for your present and future business needs
+        </TLi>
+        <TLi>Accelerate industry research in emerging technologies</TLi>
+      </ul>
+      <h5 className="text-lg font-poppins font-bold mb-6">
+        Application Design and Migration Services
+      </h5>
+      <ul className="mb-8">
+        <TLi>
+          Design, develop, deliver and maintain enticing, feature-rich user
+          applications
+        </TLi>
+        <TLi>
+          Migrate existing applications to new platforms and integrate mobile
+          solutions with pre-existing applications
+        </TLi>
+        <TLi>
+          Reduce errors with automation tools, verification methods and data
+          integrity checks
+        </TLi>
+        <TLi>Speed time to market and cross-platform development</TLi>
+      </ul>
+      <h5 className="text-lg font-poppins font-bold mb-6">
+        Testing and Maintenance Services
+      </h5>
+      <ul className="mb-8">
+        <TLi>Thoroughly test your mobile solutions</TLi>
+        <TLi>
+          Collect, consolidate and analyze mobile application data with advanced
+          data analytics and reporting tools
+        </TLi>
+      </ul>
+      <h5 className="text-lg font-poppins font-bold mb-6">
+        Infrastructure Management Services
+      </h5>
+      <ul className="mb-8">
+        <TLi>Host, remotely monitor and assure security compliance</TLi>
+        <TLi>
+          Manage user access and handle data backup, archival and reporting
+          tasks
+        </TLi>
+      </ul>
+    </div>
+  );
 };
 
 export default MSService;
