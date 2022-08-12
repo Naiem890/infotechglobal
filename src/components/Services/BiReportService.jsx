bef383657f67db7769c981d6675894507738b578
Naiem890 <solaiman.naiem890@gmail.com>
2022-08-12 15:50:06 +0600

    mobile solution details page done

--- src/components/Services/BiReportService.jsx
+++ src/components/Services/BiReportService.jsx
@@ -1,7 +1,150 @@
 import React from "react";
+import TLi from "../Shared/TLi";
 
 const BiReportService = () => {
-  return <div>bi repo</div>;
+  return (
+    <div className="mt-10">
+      <h2 className="text-4xl font-poppins font-bold mb-7">
+        Business Intelligence & Reporting
+      </h2>
+      <div className="mb-8 leading-7">
+        In today’s competitive market, in order to achieve growth and success
+        companies need realistic insights about their business. Companies need
+        to analyze data from multiple sources and correlate this data to get
+        some meaningful information which will help to drive the business.
+      </div>
+      <h3 className="text-[28px] font-poppins font-bold mb-6">
+        Benefits of Business Intelligence Analytics:
+      </h3>
+      <ul className="mb-8">
+        <TLi>
+          Create insightful reports for better strategic business decisions
+        </TLi>
+        <TLi>Share real-time information across the organization</TLi>
+        <TLi>
+          Eliminate inaccurate, time-consuming data consolidation processes
+          across multiple systems
+        </TLi>
+      </ul>
+      <h3 className="text-[28px] font-poppins font-bold mb-6">challenges</h3>
+      <div className="mb-8 leading-7">
+        Today organizations are turning to the vast pools of corporate
+        information residing in their collective networks to unlock key trends
+        and make better decisions. Many organizations are recognizing the need
+        for delivery of accurate, consistent and timely information to analysts,
+        managers, customers and other stakeholders. But many of them fail to
+        meet these objectives for many reasons, such as multiple legacy
+        reporting systems and complex spreadsheets or a failure to properly
+        implement new reporting tools.
+      </div>
+      <h3 className="text-[28px] font-poppins font-bold mb-6">Offerings</h3>
+
+      <div className="mb-8 leading-7">
+        IGLIND provides a complete solution for managing multi-domain master
+        data and related relationship hierarchies, ensuring the timely delivery
+        of highly accurate and reliable data to business intelligence,
+        analytical, and reporting systems. With this single source of
+        consolidated and reliable data, analysts can trust the reports generated
+        by their systems and executives can make decisions with confidence,
+        improving business performance.
+      </div>
+      <div className="mb-8 leading-7">
+        Infotech Global Limited India. has extensive experience in providing
+        cutting edge Business Intelligence and Reporting services to clients on
+        a global platform.
+      </div>
+      <div className="mb-8 leading-7">
+        Our BI & Reporting services ensure the availability of business-critical
+        information. It makes a clear path to competitive advantage and allowing
+        customers to substantially enhance their services and profitability. Our
+        wide range of Business Intelligence and Reporting services are:
+      </div>
+      <h3 className="text-[28px] font-poppins font-bold mb-6">
+        Business Intelligence / Data Warehousing Consulting Services:
+      </h3>
+      <ul className="mb-8">
+        <TLi>Study of existing Data Warehouse Architecture</TLi>
+        <TLi>Technology / tools evaluation and recommendation</TLi>
+        <TLi>Roadmap for re-architecture/ re-engineering services</TLi>
+      </ul>
+      <h3 className="text-[28px] font-poppins font-bold mb-6">
+        Data Warehousing Services:
+      </h3>
+      <ul className="mb-8">
+        <TLi>Database management and performance tuning</TLi>
+        <TLi>Data modeling; data extraction, transformation and loading</TLi>
+        <TLi>Data Merging, Migration and implementation</TLi>
+        <TLi>
+          ETL (Extraction, Transformation and Load) and DTS (Data Transformation
+          Services)
+        </TLi>
+        <TLi>ETL architecture and development</TLi>
+        <TLi>Development, Testing, and Deployment</TLi>
+      </ul>
+      <h3 className="text-[28px] font-poppins font-bold mb-6">
+        Reporting Services:
+      </h3>
+      <h5 className="text-lg font-poppins font-bold mb-6">
+        Crystal Reports™ reporting tools:
+      </h5>
+      <div className="mb-8 leading-7">
+        Infotech Global Limited India. has many years experience in presenting
+        data through powerful reporting tools using Crystal Reports from
+        Business Objects tools. These cater to small and mid-size enterprise
+        reporting requirements. Our solutions provide access to varying content
+        repositories, presenting and delivery within the company or externally
+        over the web.
+      </div>
+      <h3 className="text-[28px] font-poppins font-bold mb-6">Key features:</h3>
+      <h5 className="text-lg font-poppins font-bold mb-6">Data access</h5>
+      <div className="mb-8 leading-7">
+        Infotech Global Limited India. has implemented enterprise solutions
+        based on these tools that can connect to different database repositories
+        having solutions for Oracle, Microsoft and MySQL.
+      </div>
+      <h5 className="text-lg font-poppins font-bold mb-6">Presentation</h5>
+      <div className="mb-8 leading-7">
+        We provide professional looking reports through the use of visual report
+        designers and interactive prompts. Dynamic reporting, dynamic image
+        location for ability to change images in a report without having to
+        re-deploy reports, charting and mapping, hierarchical reports drill
+        down, custom functions, asymmetric reporting (provides a customized view
+        of OLAP grids to customize and hide specific dimensions for user),
+        Access and Excel add ins, and Drag drop reports.
+      </div>
+      <h5 className="text-lg font-poppins font-bold mb-6">
+        Accessibility and flexibility
+      </h5>
+      <div className="mb-8 leading-7">
+        Access to reports is available within the application, wherein data can
+        be pulled in from the server. Flexibility comes from export options to
+        various formats like PDF, Excel, Word, Access, XML, CSV, and HTML for
+        easy sharing
+      </div>
+      <h5 className="text-lg font-poppins font-bold mb-6">Security</h5>
+      <div className="mb-8 leading-7">
+        Security is implemented at an application level and system level.Other
+        features comprise of single-sign-on and role-based view of the data.
+      </div>
+      <h3 className="text-[28px] font-poppins font-bold mb-6">
+        Microsoft Reporting Services™:
+      </h3>
+      <ul className="mb-8">
+        <TLi>
+          Scalable server-based reporting platform built on the .NET Framework
+          and integrated with SQL Server 2000/2005
+        </TLi>
+        <TLi>
+          Support for parameterized reports enabling creation of custom reports.
+        </TLi>
+        <TLi>
+          Provides the end user (or business user) with a tool to create reports
+          against the Report Model facilitating ad-hoc reporting.
+        </TLi>
+        <TLi>Scheduling and subscription-based delivery of reports.</TLi>
+      </ul>
+    </div>
+  );
 };
 
 export default BiReportService;
